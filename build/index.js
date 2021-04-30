40a6802cba5f947057cee64ae512ecf355562185
Angel <angelkysinn@gmail.com>
2021-04-30 10:22:04 -0700

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hello');
+var logSomething = function () {
+    console.log('something');
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('hello');
+
+const logSomething = () => {
+  console.log('something');
+};
