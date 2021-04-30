17ef57673da2f3097424ff764486f9d9ed5055e6
Angel <angelkysinn@gmail.com>
2021-04-30 11:11:19 -0700

    Add type guard to restore access to set of properties in union type

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,27 @@ class Sorter {
   // }
 
   // equivalent to above code
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftSide = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftSide;
+        // if collection is an array of numbers, do this:
+        // use type guard - checks if array
+        if (this.collection instanceof Array) {
+          // collection === number[]
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftSide = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftSide;
+          }
+        }
+
+        // if collection is a string, do this logic instead:
+        // use type guard - checks if string
+        if (typeof this.collection === 'string') {
         }
       }
     }
