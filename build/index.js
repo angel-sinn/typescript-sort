ff3f8d4c8ff22c2f696806c084b0ff5da07f3560
Angel <angelkysinn@gmail.com>
2021-04-30 10:53:35 -0700

    Add sort algorithm for array of numbers

--- build/index.js
+++ build/index.js
@@ -1,5 +1,27 @@
 "use strict";
-console.log('123');
-var logSomething = function () {
-    console.log('something');
-};
+var Sorter = /** @class */ (function () {
+    // collection: number[]; // field definition
+    // constructor(collection: number[]) {
+    //   this.collection = collection; // assignment
+    // }
+    // equivalent to above code
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftSide = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftSide;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
